c80d436c4a0a2a2f1e1ad9fdecb1201d90ce7c2a
raum-dellamorte <elementalsin@gmail.com>
2026-03-24 01:05:39 -0800

    updated obs-wrapper bindings in fork to 32.0.4

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 
 
 fn main() {
-  let proj_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-  println!("cargo:rustc-link-search=native={}/deps", proj_dir);
+  if std::env::var("CARGO_CFG_TARGET_OS").map(|s| s.contains("windows") ).unwrap_or(false) {
+    let proj_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}/deps", proj_dir);
+  }
 }
